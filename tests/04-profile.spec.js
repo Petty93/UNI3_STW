1901347a0db65a209281290378733c5e14c460f6
Petty <npthanh.131@gmail.com>
2025-08-05 10:40:36 +0700

    Update upload_files folder & test 04

--- tests/04-profile.spec.js
+++ tests/04-profile.spec.js
@@ -1,26 +1,20 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login';
+const path = require('path')
 
 test('Truy cập profile', async ({ page }) => {
   const Login1 = new LoginPage(page)
   await Login1.gotoLoginPage()
   await Login1.login('thanhnp.sv93', '1')
-
-  // await page.getByRole('img', { name: 'Avatar' }).first().click();
-  // await expect(page.getByAltText('Avatar')).toBeVisible({ timeout: 5000 });
-  // await page.getByAltText('Avatar').click();
-
-await page.pause();
-await page.locator('img[alt="Avatar"]').click();
-
-
-
+  await page.getByRole('img', { name: 'Avatar' }).first().click();
   await page.getByText('Hồ sơ').click();
-  await page.locator('div').filter({ hasText: /^ThanhNP SV 93Đổi mật khẩu$/ }).locator('i').click();
-  await page.locator('body').setInputFiles('th.jpg');
-  await page.getByRole('button', { name: 'Đổi mật khẩu' }).click();
+  const filePath = path.resolve(__dirname, '../upload_files/th.jpg');
+  const fileInput = page.locator('.inputfile');
+  await fileInput.setInputFiles(filePath);
+  await expect(page.locator('text=Đổi hình đại diện thành công')).toBeVisible();
+  await page.locator('.uni-btn-type-2.btn.btn-primary.btn-lg').click();
   await page.locator('div').filter({ hasText: /^Mật khẩu$/ }).getByPlaceholder('Matkhau@').fill('Hello@123');
   await page.locator('div').filter({ hasText: /^Nhập lại mật khẩu$/ }).getByPlaceholder('Matkhau@').fill('Hello@123');
-  await page.getByRole('button', { name: 'Xác nhận' }).click();
-  await page.getByText('Xác nhận').click();
+  await expect(page.locator('.uni-btn-type-1.btn.btn-primary')).toBeVisible()
+  await page.locator('.uni-btn-type-1.btn.btn-primary').click();
 });
